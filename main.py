8af3d6f23f74bb55011431d5deea73397247e141
Dominic Lees <dxl184@student.bham.ac.uk>
2026-03-31 17:08:12 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Goalie Tracker")
+
+# Create root window size of screen
+root.geometry("{0}x{1}".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+
+root.mainloop()
