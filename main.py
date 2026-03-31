0e0372ed20df5bc5cd40aea1221999bbe34cb76c
Dominic Lees <dxl184@student.bham.ac.uk>
2026-03-31 17:25:48 +0100

    Added new button

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ root.title("Goalie Tracker")
 root.geometry("{0}x{1}".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
 # Create sidebar
-sidebar = Frame(root)
+sidebar = Frame(root, highlightthickness=1, highlightbackground="black")
 sidebar.grid(row=0, column=0)
+sidebar.place(relwidth=0.2, relheight=1)
 
 # Create main area
 main = Frame(root)
-main.grid(row=0, column=1, columnspan=4)
+main.grid(row=0, column=1, columnspan=4, sticky=E)
+main.place(relx=0.2, relwidth=0.8, relheight=1)
+
+# Create new button
+newBtn = Button(main, text="New")
+newBtn.grid(row=0, column=0)
 
 root.mainloop()
