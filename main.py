81bcd6751effddddcc96d545200858306ba7ed6e
Dominic Lees <dxl184@student.bham.ac.uk>
2026-03-31 17:14:07 +0100

    Added sidebar and main frames

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ root.title("Goalie Tracker")
 # Create root window size of screen
 root.geometry("{0}x{1}".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
+# Create sidebar
+sidebar = Frame(root)
+sidebar.grid(row=0, column=0)
+
+# Create main area
+main = Frame(root)
+main.grid(row=0, column=1, columnspan=4)
+
 root.mainloop()
