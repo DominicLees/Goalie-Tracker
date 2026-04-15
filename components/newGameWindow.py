fcf028c97ef76331d7974578885bc72d9a2bdd12
Dominic Lees <leesdominic@gmail.com>
2026-04-15 08:50:28 +0100

    Refactored newGameWindow to be a child class of TopLevel

--- components/newGameWindow.py
+++ components/newGameWindow.py
@@ -1,17 +1,26 @@
 from tkinter import Button, Entry, Toplevel
 from typing import Callable
 
-class NewGameWindow:
-    def __init__(self, cb: Callable[[str], None]):
-        win = Toplevel()
-        win.wm_title("New Game")
+class NewGameWindow(Toplevel):
+    def __init__(self, cb: Callable[[str], None], text=""):
+        super().__init__()
+        self.cb = cb
+        self.wm_title("New Game")
 
         # Create text box for new game
-        newGameTextbox = Entry(win)
-        newGameTextbox.grid(row=0, column=0)
-        newGameTextbox.bind("<Return>", lambda self: [cb(newGameTextbox.get()), win.destroy()])
-        newGameTextbox.focus()
+        self.newGameTextbox = Entry(self, text=text)
+        self.newGameTextbox.grid(row=0, column=0)
+        self.newGameTextbox.bind("<Return>", self.submit)
+        self.newGameTextbox.focus()
 
         # Create submit button
-        submit = Button(win, text="Submit", command=lambda: [cb(newGameTextbox.get()), win.destroy()])
+        submit = Button(self, text="Submit", command=self.submit)
         submit.grid(row=0, column=1)
+
+    def submit(self):
+        name = self.newGameTextbox.get()
+        if len(name) == 0:
+            return
+        
+        self.cb(name)
+        self.destroy()
