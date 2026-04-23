180215a50f2d078d2b889e7048c0c1061519413a
Dominic Lees <leesdominic@gmail.com>
2026-04-23 13:38:26 +0100

    Added docstrings to db.py

--- utils/db.py
+++ utils/db.py
@@ -11,11 +11,28 @@ cu.execute("""CREATE TABLE IF NOT EXISTS Games(
 )""")
 db.commit()
 
-# TODO: docstrings
+# Database functions
+
 def getAllGames(fields="*"):
+    """Retrieves all games from the database.
+
+    Args:
+        fields (str, optional): Which fields to get. Defaults to "*".
+
+    Returns:
+        List[Tuple[str]]: Returns all records in a list of tuples.
+    """
     return cu.execute("SELECT {0} FROM Games".format(fields)).fetchall()
 
 def getGame(name: str) -> Game | None:
+    """Retrieves a game from the database from its name
+
+    Args:
+        name (str): The name of the game to retrieve
+
+    Returns:
+        Game | None: If the game was found returns a Game object, otherwise returns None
+    """
     cu.execute("Select * FROM Games WHERE name = ?", (name,))
     result = cu.fetchone()
     if result == None:
@@ -23,6 +40,14 @@ def getGame(name: str) -> Game | None:
     return Game(result)
 
 def insertNewGame(name: str) -> bool:
+    """Inserts a new game with the provided name into the database
+
+    Args:
+        name (str): The value of the name field to be inserted
+
+    Returns:
+        bool: True if insert was successful, otherwise false
+    """
     if getGame(name) != None:
         return False
     try:
@@ -34,6 +59,14 @@ def insertNewGame(name: str) -> bool:
         return False
 
 def updateGame(game: Game) -> bool:
+    """Takes a game object and updates the corresponding record in the database to match the current values.
+
+    Args:
+        game (Game): Game instance to update the database with. A game record with the same name must already exist in the database.
+
+    Returns:
+        bool: True if update was successful, otherwise false
+    """
     try:
         cu.execute("UPDATE Games SET shots = ?, goals = ? WHERE name = ?", (game.shots, game.goals, game.name))
         db.commit()
@@ -43,6 +76,15 @@ def updateGame(game: Game) -> bool:
         return False
     
 def updateGameName(oldName: str, newName: str) -> bool:
+    """Changes the name of a game record in the database
+
+    Args:
+        oldName (str): The name of the record in the database
+        newName (str): The value to change the game's name to
+
+    Returns:
+        bool: True if update was successful, otherwise false
+    """
     try:
         cu.execute("UPDATE Games SET name = ? WHERE name = ?", (newName, oldName))
         db.commit()
