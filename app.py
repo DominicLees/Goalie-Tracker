f6e0d7046ac9e904fc438d029175d72f692ffa9d
Dominic Lees <leesdominic@gmail.com>
2026-04-24 15:21:49 +0100

    Moved sidebar population to be after creation of the menu bar

--- app.py
+++ app.py
@@ -18,25 +18,27 @@ sidebar = Sidebar(root, highlightthickness=1, highlightbackground="black")
 sidebar.grid(row=0, column=0)
 sidebar.place(relwidth=0.2, relheight=1)
 sidebar.main = main
-# Populate sidebar
-games = getAllGames("name")
-for game in games:
-    sidebar.createGameButton(game[0])
 
 # Create menu bar
 menubar = Menu(root)
 file = Menu(menubar, tearoff=0)
 main.fileMenu = file
 menubar.add_cascade(label ="File", menu=file)
-# TODO: Move NewGameWindow handling to sidebar
+# New Game
 file.add_command(label ="New Game", accelerator="Cmd+N", command=lambda: NewGameWindow(sidebar.newGame))
 root.bind("<Command-n>", lambda self: NewGameWindow(sidebar.newGame))
+# Rename Game
 file.add_command(label ="Rename Game", accelerator="Cmd+R", command=lambda: NewGameWindow(sidebar.renameGame, main.game.name, "Rename Game"), state="disabled")
 root.bind("<Command-r>", lambda self: NewGameWindow(sidebar.renameGame, main.game.name, "Rename Game"))
 root.config(menu = menubar)
 
+# Populate sidebar
+games = getAllGames("name")
+for game in games:
+    sidebar.createGameButton(game[0])
 # Open first game in db
 if (len(games) > 0):
     main.openGame(games[0][0])
+del games
 
 root.mainloop()
