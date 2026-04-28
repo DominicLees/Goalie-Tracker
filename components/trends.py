9e78bdb391e267ca0ba3b1ec9b8bfc95cdaca9b9
Dominic Lees <leesdominic@gmail.com>
2026-04-28 15:00:32 +0100

    Added Trends.average()

--- components/trends.py
+++ components/trends.py
@@ -1,5 +1,6 @@
 from tkinter import Event, Frame, Label
-from utils.db import getAllGames
+from typing import Tuple
+from utils.db import GameRecords, getAllGames
 
 class Trends(Frame):
     """The trends tab that displays data calculated from all of the logged games"""
@@ -22,3 +23,14 @@ class Trends(Frame):
         
         games = getAllGames()
         Label(self, text="{0} games logged".format(len(games))).grid()
+        Label(self, text="Average shots against: {0}".format(self.average(games, 1))).grid(row=1)
+        Label(self, text="Average goals against: {0}".format(self.average(games, 2))).grid(row=2)
+
+    def average(self, values: GameRecords, pos: int) -> float:
+        recordCount = len(values)
+        if recordCount < 1:
+            return 0
+        total = 0
+        for game in values:
+            total += game[pos]
+        return total / len(values)
