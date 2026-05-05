49c30733f235150f1769beaebd6e957b48805607
Dominic Lees <leesdominic@gmail.com>
2026-05-05 15:31:49 +0100

    Added trends.createGraph() and graphs for Goals Against, Saves and Save% to trends tab

--- components/trends.py
+++ components/trends.py
@@ -1,4 +1,5 @@
 from tkinter import Event, Frame, Label, Misc
+from tkinter.ttk import Notebook
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from utils.db import GameRecords, getAllGames
@@ -58,18 +59,18 @@ class Trends(Frame):
             child.destroy()
         
         # Get all games and sort by date
-        games = getAllGames()
-        games.sort(key=lambda game: game[3])
+        self.games = getAllGames()
+        self.games.sort(key=lambda game: game[3])
         # Calculate totals
-        totalShots = total(games, 1)
-        totalGoals = total(games, 2)
+        totalShots = total(self.games, 1)
+        totalGoals = total(self.games, 2)
         # Calculate averages
-        averageShots = average(games, 1)
-        averageGoals = average(games, 2)
+        averageShots = average(self.games, 1)
+        averageGoals = average(self.games, 2)
         averageSaves = averageShots - averageGoals
         
         # Create total games label
-        Label(self, text="{0} games logged".format(len(games))).grid(columnspan=2)
+        Label(self, text="{0} games logged".format(len(self.games))).grid(columnspan=2)
         
         # Create averages labels
         Label(self, text="Average shots against per game: {0}".format(averageShots)).grid(row=1)
@@ -81,19 +82,54 @@ class Trends(Frame):
         Label(self, text="Total goals against: {0}".format(totalGoals)).grid(row=2, column=1)
         Label(self, text="Total saves: {0}".format(totalShots - totalGoals)).grid(row=3, column=1)
 
+        graphTabs = Notebook(self)
+        graphTabs.grid(row=4, columnspan=2)
+        graphTabs.add(Frame(graphTabs), text="Shots Against")
+        graphTabs.add(Frame(graphTabs), text="Goals Against")
+        graphTabs.add(Frame(graphTabs), text="Saves")
+        graphTabs.add(Frame(graphTabs), text="Save%")
+        graphTabs.bind("<<NotebookTabChanged>>", self.createGraph)
+
+    def createGraph(self, event: Event):
+        """Creates the graph for the data for the currently selected tab on the graphTab Notebook.
+
+        Args:
+            event (Event): The event object passed to the function.
+        """
+        # Check the trends tab was selected before continuing
+        selected_tab = event.widget.select()
+        tab_text = event.widget.tab(selected_tab, "text")
+        tab_frame = event.widget.nametowidget(selected_tab)
+
+        # If graph has already been drawn do not plot it again
+        if len(tab_frame.winfo_children()) > 0:
+            return
+
         # Create the figure that will contain the graph
         fig = Figure(figsize = (10, 5), dpi = 100)
 
-        # TODO: Add the ability to select different graphs
         # Get coordinates
-        y = list(map(lambda game: game[1], games))
-        x = [i+1 for i in range(len(games))]
-
+        x = []
+        y = []
+        match tab_text:
+            case "Shots Against":
+                y = list(map(lambda game: game[1], self.games))
+                x = [i+1 for i in range(len(self.games))]
+            case "Goals Against":
+                y = list(map(lambda game: game[2], self.games))
+                x = [i+1 for i in range(len(self.games))]
+            case "Saves":
+                y = list(map(lambda game: game[1] - game[2], self.games))
+                x = [i+1 for i in range(len(self.games))]
+            case "Save%":
+                y = list(map(lambda game: ((game[1] - game[2]) / game[1]), self.games))
+                x = [i+1 for i in range(len(self.games))]
+        
         # Plot the graph
         graph = fig.add_subplot()
         graph.plot(x, y)
 
         # Placing the graph in the window
-        canvas = FigureCanvasTkAgg(fig, master = self)  
+        canvas = FigureCanvasTkAgg(fig, tab_frame)  
         canvas.draw()
-        canvas.get_tk_widget().grid(row=4, columnspan=2)
+        canvas.get_tk_widget().pack(fill="both", expand=True)
